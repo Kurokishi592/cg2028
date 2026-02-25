fb9f01fc5d8a87c237ea7ea5c57e585c2d5b5ffd
Kurokishi592 <kennethwongcunwi@gmail.com>
2026-02-25 18:09:49 +0800

    asm for mov_avg.s passed all public test cases. Amend test code as well for better tests

--- Project Assignment/CG2028_Assignment_Test/Core/Src/main.c
+++ Project Assignment/CG2028_Assignment_Test/Core/Src/main.c
@@ -34,9 +34,9 @@ int main(void)
 
 	/*********************************************************/
 
-	int sensor_data_x[16]={1000,1001,1002,1003,1004,1005,1006,1007,1008,1009,1010,1011,1012,1013,1014,1015};
-	int sensor_data_y[16]={1016.1017,1018,1019,1020,1021,1022,1023,1024,1025,1026,1027,1028,1029.1030,1031};
-	int sensor_data_z[16]={1032,1033,1034,1035,1036,1037,1038,1039,1040,1041,1042,1043,1044,1045,1046,1047};
+	int sensor_data_x[16]={6030,6000,4389,4488,6734,5009,7318,6040,5000,5643,3888,3488,3488,3488,4876,5010};
+	int sensor_data_y[16]={9800,9800,6573,6753,5004,5895,4321,6753,9004,3005,6934,4444,7981,5144,5867,4178};
+	int sensor_data_z[16]={5455,6177,8653,7777,6520,4566,7860,5199,6233,5444,9822,3455,6445,7888,4113,4998};
 
 
 	/*********************************************************/
@@ -78,16 +78,15 @@ int main(void)
 		filt_avg_asm[0]=mov_avg(N,accel_buff_x);
 		filt_avg_asm[1]=mov_avg(N,accel_buff_y);
 		filt_avg_asm[2]=mov_avg(N,accel_buff_z);
-
+		printf("Expected result (X): %d, Current result: %d \n", filt_avg_c[0],filt_avg_asm[0]);
+					printf("Expected result (Y): %d, Current result: %d \n", filt_avg_c[1],filt_avg_asm[1]);
+					printf("Expected result (Z): %d, Current result: %d \n", filt_avg_c[2],filt_avg_asm[2]);
 		//Check whether the returned values of the assembly and the C code are the same
 		if( (filt_avg_asm[0]!=filt_avg_c[0]) ||
-			(filt_avg_asm[2]!=filt_avg_c[2]) ||
-			(filt_avg_asm[3]!=filt_avg_c[3]) )
+			(filt_avg_asm[1]!=filt_avg_c[1]) ||
+			(filt_avg_asm[2]!=filt_avg_c[2]) )
 		{
 			printf("Assembly and C values mismatch\n");
-			printf("Expected result (X): %d, Current result: %d \n", filt_avg_c[0],filt_avg_asm[0]);
-			printf("Expected result (Y): %d, Current result: %d \n", filt_avg_c[1],filt_avg_asm[1]);
-			printf("Expected result (Z): %d, Current result: %d \n", filt_avg_c[2],filt_avg_asm[2]);
 			flag=0;
 			break;
 		}
--- Project Assignment/CG2028_Assignment_Test/Core/Src/mov_avg.s
+++ Project Assignment/CG2028_Assignment_Test/Core/Src/mov_avg.s
@@ -2,8 +2,14 @@
  * mov_avg.s
  *
  * Created on: 2/2/2026
- * Author: Hitesh B, Hou Linxin
+ * Author: Hitesh B, Hou Linxin\
+ *
+ * CG2028 Assignment, Sem 2, AY 2025/26
+ * (c) ECE NUS, 2025
+ * Write Student 1’s Name here: Kenneth Wong Cun Wi (A0303203A)
+ * Write Student 2’s Name here: Joel Ku (A0308792U)
  */
+
 .syntax unified
  .cpu cortex-m4
  .thumb
@@ -11,18 +17,40 @@
  .equ N_MAX, 8
  .bss
  .align 4
-
  .text
  .align 2
-@ CG2028 Assignment, Sem 2, AY 2025/26
-@ (c) ECE NUS, 2025
-@ Write Student 1’s Name here: ABCD (A1234567R)
-@ Write Student 2’s Name here: WXYZ (A0000007X)
-@ You could create a look-up table of registers here:
-@ R0 ...
-@ R1 ...
-@ write your program from here:
-mov_avg:
- PUSH {r2-r11, lr}
 
- POP {r2-r11, pc}
+/*
+ * brief: this moving average filter calculates the average of the most recent N readings from accel_buffer
+ * 
+ * inputs:
+ * R0 N (buffer size)
+ * R1 accel_buff (pointer to an int buffer containing the most recent N accelerometer readings)
+ *
+ * Temp reg:
+ * R2 current sum of readings thus far
+ * R3 Loop counter i
+ * R4 current reading from buffer accel_buffer[R1]
+ *
+ * Outputs:
+ * R0 average value of the N readings in the buffer
+ */
+
+mov_avg:
+ PUSH {r2-r4, lr} @ push regs to be modified and return addr in LR onto stack
+
+ MOV r2, #0      @ init sum = 0
+ MOV r3, #0      @ init i = 0
+
+loop:
+ CMP r3, r0      @ compare loop counter i and buffer size N, set condition flags
+ BGE end_loop   @ exit when i >= N  (negation of while loop cond. i < N)
+
+ LDR r4, [r1], #4   @ post index to load next value from buffer into R4 and increment buffer pointer
+ ADD r2, r2, r4   @ add value to current sum thus far
+ ADD r3, r3, #1      @ increment loop counter i++
+ B loop         @ branch back to loop for next index in buffer
+
+end_loop:
+ SDIV r0, r2, r0  @ average = sum / N, store result in return reg R0 to return to caller 
+ POP {r2-r4, pc} @ pop regs to restore their original values and the return addr in LR into PC to return
