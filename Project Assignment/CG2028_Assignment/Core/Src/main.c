ff8cde1ac60c71e42c915acb28d3bb594f02e2e0
joeLku1 <joelku77@gmail.com>
2026-02-25 14:07:15 +0800

    Fix formattting issues

--- Project Assignment/CG2028_Assignment/Core/Src/main.c
+++ Project Assignment/CG2028_Assignment/Core/Src/main.c
@@ -4,8 +4,8 @@
   * (c) CG2028 Teaching Team
   ******************************************************************************/
 
-
-/*--------------------------- Includes ---------------------------------------*/
+/* ---------------------------------------------------------------------------------------------------------- */
+/* ------------------------------------------- Includes ----------------------------------------------------- */
 #include "main.h"
 #include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_accelero.h"
 #include "../../Drivers/BSP/B-L4S5I-IOT01/stm32l4s5i_iot01_tsensor.h"
@@ -30,72 +30,69 @@ int main(void)
 {
 	const int N=4;
 
-	/* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-	HAL_Init();
+	HAL_Init();													// Reset all peripherals, initialize flash interface and systick
 
-	/* UART initialization  */
-	UART1_Init();
+	UART1_Init();												// Initialize UART1 for serial communication
 
-	/* Peripheral initializations using BSP functions */
+	// Peripheral initializations using BSP functions
 	BSP_LED_Init(LED2);
 	BSP_ACCELERO_Init();
 	BSP_GYRO_Init();
 
-	/*Set the initial LED state to off*/
-	BSP_LED_Off(LED2);
+	BSP_LED_Off(LED2);											// Set the initial LED state to off
 
 	int accel_buff_x[4]={0};
 	int accel_buff_y[4]={0};
 	int accel_buff_z[4]={0};
-	int i=0;
-	int delay_ms=1000; //change delay time to suit your code
+	int i=0;													// Counter to keep track of how many readings have been taken
+	int delay_ms=1000; 											// Change delay time to suit your code
 
 	while (1)
 	{
+		BSP_LED_Toggle(LED2);									// This function helps to toggle the current LED state
 
-		BSP_LED_Toggle(LED2);		// This function helps to toggle the current LED state
+		int16_t accel_data_i16[3] = { 0 };						// Array to store the x, y and z readings of accelerometer
+		
+		BSP_ACCELERO_AccGetXYZ(accel_data_i16);					// Function to read accelerometer values
 
-		int16_t accel_data_i16[3] = { 0 };			// array to store the x, y and z readings of accelerometer
-		/********Function call to read accelerometer values*********/
-		BSP_ACCELERO_AccGetXYZ(accel_data_i16);
-
-		//Copy the values over to a circular style buffer
-		accel_buff_x[i%4]=accel_data_i16[0]; //acceleration along X-Axis
-		accel_buff_y[i%4]=accel_data_i16[1]; //acceleration along Y-Axis
-		accel_buff_z[i%4]=accel_data_i16[2]; //acceleration along Z-Axis
+		// Copy the values over to a circular style buffer
+		accel_buff_x[i%4]=accel_data_i16[0]; 					// Acceleration along X-Axis
+		accel_buff_y[i%4]=accel_data_i16[1]; 					// Acceleration along Y-Axis
+		accel_buff_z[i%4]=accel_data_i16[2]; 					// Acceleration along Z-Axis
 
 
-		// ********* Read gyroscope values *********/
 		float gyro_data[3]={0.0};
 		float* ptr_gyro=gyro_data;
-		BSP_GYRO_GetXYZ(ptr_gyro);
+		BSP_GYRO_GetXYZ(ptr_gyro);								// Function to read gyroscope values
 
-		//The output of gyro has been made to display in dps(degree per second)
+		// The output of gyro has been made to display in dps(degree per second)
 		float gyro_velocity[3]={0.0};
 		gyro_velocity[0]=(gyro_data[0]*9.8/(1000));
 		gyro_velocity[1]=(gyro_data[1]*9.8/(1000));
 		gyro_velocity[2]=(gyro_data[2]*9.8/(1000));
 
 
-		//Preprocessing the filtered outputs  The same needs to be done for the output from the C program as well
-		float accel_filt_asm[3]={0}; // final value of filtered acceleration values
+		// Preprocessing the filtered outputs  The same needs to be done for the output from the C program as well
+		float accel_filt_asm[3]={0}; 							// final value of filtered acceleration values
 
 		accel_filt_asm[0]= (float)mov_avg(N,accel_buff_x) * (9.8/1000.0f);
 		accel_filt_asm[1]= (float)mov_avg(N,accel_buff_y) * (9.8/1000.0f);
 		accel_filt_asm[2]= (float)mov_avg(N,accel_buff_z) * (9.8/1000.0f);
 
 
-		//Preprocessing the filtered outputs  The same needs to be done for the output from the assembly program as well
+		// Preprocessing the filtered outputs  The same needs to be done for the output from the assembly program as well
 		float accel_filt_c[3]={0};
 
 		accel_filt_c[0]=(float)mov_avg_C(N,accel_buff_x) * (9.8/1000.0f);
 		accel_filt_c[1]=(float)mov_avg_C(N,accel_buff_y) * (9.8/1000.0f);
 		accel_filt_c[2]=(float)mov_avg_C(N,accel_buff_z) * (9.8/1000.0f);
 
-		/***************************UART transmission*******************************************/
-		char buffer[150]; // Create a buffer large enough to hold the text
+		/* ---------------------------------------------------------------------------------------------------------- */
+		// UART transmission of the results
 
-		/******Transmitting results of C execution over UART*********/
+		char buffer[150]; 									// Create a buffer large enough to hold the text
+
+		// Transmitting results of C execution over UART
 		if(i>=3)
 		{
 			// 1. First printf() Equivalent
@@ -108,7 +105,7 @@ int main(void)
 					accel_filt_c[0], accel_filt_c[1], accel_filt_c[2]);
 			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 
-			/******Transmitting results of asm execution over UART*********/
+			// Transmitting results of asm execution over UART
 
 			// 1. First printf() Equivalent
 			sprintf(buffer, "Results of assembly execution for filtered accelerometer readings:\r\n");
@@ -120,7 +117,7 @@ int main(void)
 					accel_filt_asm[0], accel_filt_asm[1], accel_filt_asm[2]);
 			HAL_UART_Transmit(&huart1, (uint8_t*)buffer, strlen(buffer), HAL_MAX_DELAY);
 
-			/******Transmitting Gyroscope readings over UART*********/
+			// Transmitting Gyroscope readings over UART
 
 			// 1. First printf() Equivalent
 			sprintf(buffer, "Gyroscope sensor readings:\r\n");
@@ -137,60 +134,75 @@ int main(void)
 
 		i++;
 
-		// ********* Fall detection *********/
+		/* ---------------------------------------------------------------------------------------------------------- */
+		// Fall detection
 		// write your program from here:
 
 
 	}
-
-
 }
 
 
-
+/**
+ * @brief  C implementation of the moving average filter. This is a reference implementation and is not optimized for performance.
+ * @param  N: Number of samples to average (window size)
+ * @param  accel_buff: Pointer to the buffer containing the accelerometer readings
+ * @return The average of the last N samples in the buffer
+ */
 int mov_avg_C(int N, int* accel_buff)
 { 	// The implementation below is inefficient and meant only for verifying your results.
-	int result=0;
-	for(int i=0; i<N;i++)
+	int result = 0;
+	for(int i = 0; i < N; i ++)
 	{
-		result+=accel_buff[i];
+		result += accel_buff[i];
 	}
-
-	result=result/4;
-
+	result = result / 4;
 	return result;
 }
 
+/**
+ * @brief  UART1 Initialization Function
+ * Pin configuration for UART. BSP_COM_Init() can do this automatically.
+ */
 static void UART1_Init(void)
 {
-        /* Pin configuration for UART. BSP_COM_Init() can do this automatically */
-        __HAL_RCC_GPIOB_CLK_ENABLE();
-         __HAL_RCC_USART1_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
+	__HAL_RCC_USART1_CLK_ENABLE();
 
-        GPIO_InitTypeDef GPIO_InitStruct = {0};
-        GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
-        GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_6;
-        GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
-        GPIO_InitStruct.Pull = GPIO_NOPULL;
-        GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
-        HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+	GPIO_InitTypeDef GPIO_InitStruct = {0};
+	GPIO_InitStruct.Alternate = GPIO_AF7_USART1;
+	GPIO_InitStruct.Pin = GPIO_PIN_7|GPIO_PIN_6;
+	GPIO_InitStruct.Mode = GPIO_MODE_AF_PP;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_VERY_HIGH;
+	HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-        /* Configuring UART1 */
-        huart1.Instance = USART1;
-        huart1.Init.BaudRate = 115200;
-        huart1.Init.WordLength = UART_WORDLENGTH_8B;
-        huart1.Init.StopBits = UART_STOPBITS_1;
-        huart1.Init.Parity = UART_PARITY_NONE;
-        huart1.Init.Mode = UART_MODE_TX_RX;
-        huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
-        huart1.Init.OverSampling = UART_OVERSAMPLING_16;
-        huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
-        huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
-        if (HAL_UART_Init(&huart1) != HAL_OK)
-        {
-          while(1);
-        }
+	/* Configuring UART1 */
+	huart1.Instance = USART1;
+	huart1.Init.BaudRate = 115200;
+	huart1.Init.WordLength = UART_WORDLENGTH_8B;
+	huart1.Init.StopBits = UART_STOPBITS_1;
+	huart1.Init.Parity = UART_PARITY_NONE;
+	huart1.Init.Mode = UART_MODE_TX_RX;
+	huart1.Init.HwFlowCtl = UART_HWCONTROL_NONE;
+	huart1.Init.OverSampling = UART_OVERSAMPLING_16;
+	huart1.Init.OneBitSampling = UART_ONE_BIT_SAMPLE_DISABLE;
+	huart1.AdvancedInit.AdvFeatureInit = UART_ADVFEATURE_NO_INIT;
+	if (HAL_UART_Init(&huart1) != HAL_OK)
+	{
+		while(1);
+	}
 
+}
+
+/**
+ * Helper function for LED blinking
+ * @param delay_ms: Delay in milliseconds between toggles
+ */
+void blink_LED2(int delay_ms)
+{
+	BSP_LED_Toggle(LED2);
+	HAL_Delay(delay_ms);
 }
 
 
